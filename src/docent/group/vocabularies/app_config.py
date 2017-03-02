1d57c75eb2aed0a6295cbf37229adab954afedba
Luke Brannon <luke.brannon@gmail.com>
2017-03-01 18:08:05 -0800

    added Training Members group

--- src/docent/group/vocabularies/app_config.py
+++ src/docent/group/vocabularies/app_config.py
@@ -5,6 +5,7 @@ ADVISORS_GROUP_ID = 'Advisors'
 EXECUTIVE_COMMITTEE_GROUP_ID = 'Executive_Committee'
 LWHS_STAFF_MEMBERS_GROUP_ID = 'LWHS_Staff_Members'
 TRAINED_MEMBERS_GROUP_ID = 'Trained_Members'
+TRAINING_MEMBERS_GROUP_ID = 'Training_Members'
 BOOSTER_MEMBERS_GROUP_ID = 'Booster_Members'
 
 GROUP_ID_DICT = {BOOSTER_BOARD_MEMBERS_GROUP_ID: 'Booster Board Members',
@@ -12,4 +13,5 @@ GROUP_ID_DICT = {BOOSTER_BOARD_MEMBERS_GROUP_ID: 'Booster Board Members',
                  EXECUTIVE_COMMITTEE_GROUP_ID: 'Executive Committee',
                  LWHS_STAFF_MEMBERS_GROUP_ID: 'LWHS Staff Members',
                  TRAINED_MEMBERS_GROUP_ID: 'Trained Members',
+                 TRAINING_MEMBERS_GROUP_ID: 'Training Members',
                  BOOSTER_MEMBERS_GROUP_ID: 'Booster Members',}
